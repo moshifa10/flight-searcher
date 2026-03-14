7323d2772cf14a49d684c6ceb91a7e6ac869a57c
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-03-15 01:13:07 +0200

    added a class called exhange rate to exchange the rate

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,36 @@
+import smtplib
+from email.message import EmailMessage
+import dotenv
+import os
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        dotenv.load_dotenv()
+    def send_email(self, price, departure, arrival, outbound_date, inbound_date):
+        sender_email = "your_email@gmail.com"
+        receiver_email = "receiver_email@gmail.com"
+        password = os.getenv(key="PASSWORD_EMAIL")
+
+        subject = "✈️ Cheap Flight Alert!"
+        body = f"""
+        Low price alert!
+
+        Flight from {departure} to {arrival}
+
+        Price: ${price}
+        Departure date: {outbound_date}
+        Return date: {inbound_date}
+
+        Book quickly before the price increases!
+        """
+
+        msg = EmailMessage()
+        msg["Subject"] = subject
+        msg["From"] = sender_email
+        msg["To"] = receiver_email
+        msg.set_content(body)
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(sender_email, password)
+            connection.send_message(msg)

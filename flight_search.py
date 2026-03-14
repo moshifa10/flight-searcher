810100e32dd431a2209e28da07c8b0f82d8765b3
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-03-14 18:20:00 +0200

    updated: updated the iatcode if it doesnt have it just update with Testing for now then later will change it

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,14 @@
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    
+    def put_testing(self, data:dict) -> dict:
+        '''
+            This function is responsible for checking each an every data passed in has the iataCode available if not just pass in Testing for that data.
+        '''
+        code = data["iataCode"]
+        if len(code) == 0:
+            data["iataCode"] = "Testing"
+        return data
+    
+

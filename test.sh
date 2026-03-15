826d73b6f4452a47c70646aa049d6820d2502a9c
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-03-15 03:07:22 +0200

    intergrated with google forms to accept users if they want to be in the programme

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ echo "Hello I am Njabs"
 echo "I will be running it using shell"
 
 git add data_manager.py main.py requirements.txt flight_search.py flight_data.py exchange_rate.py notification_manager.py test.sh
-git commit -m "I am done with the project"
+git commit -m "intergrated with google forms to accept users if they want to be in the programme"
 git push origin main
